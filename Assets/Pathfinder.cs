b7376f83c45a91a48ab44200fe6ae4e965236982
LAPTOP-EPH67RN9\ASUS <dasithrathnasinghe@gmail.com>
2021-02-24 00:30:58 +0530

    added Pathfinder script for World game object

---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Dictionary<Vector2Int,Waypoint> grids =new Dictionary<Vector2Int,Waypoint>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        Waypoint[] waypoints = FindObjectsOfType<Waypoint>();
+        foreach(Waypoint waypoint in waypoints)
+        {
+            if (grids.ContainsKey(waypoint.GetGridPos())) {
+                Debug.LogWarning(" skipping overlapping block"+waypoint.name);
+            }
+            else { grids.Add(waypoint.GetGridPos(), waypoint); }
+        }
+        print("Loaded "+grids.Count+" blocks");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
