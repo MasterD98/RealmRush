d8566f5117ec5fcde072fbbdfbf34c5fb803739a
LAPTOP-EPH67RN9\ASUS <dasithrathnasinghe@gmail.com>
2021-02-24 21:39:21 +0530

    modified waypoint.cs to carry waypoint data

--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    
+    public Waypoint exploredFrom;
+    [SerializeField] Color exploredColor=Color.blue;
     private const int gridsize = 10;
     private Vector2Int gridPos;
+    public bool isExplored = false;
 
+    private void Start()
+    {
+        
+    }
     public Vector2Int GetGridPos() {
         gridPos=new Vector2Int(
         Mathf.RoundToInt(transform.position.x / gridsize),
@@ -18,6 +24,19 @@ public class Waypoint : MonoBehaviour
     public void SetWayPointColor(Color color) {
         MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
         topMeshRenderer.material.color = color;
+    }
+
+    private void Update()
+    {
+        SetExploredWaypointColor();
+    }
+
+    private void SetExploredWaypointColor()
+    {
+        if (isExplored)
+        {
+            SetWayPointColor(exploredColor);
+        }
     }
 
     public int GetGridSize() { return gridsize; }
