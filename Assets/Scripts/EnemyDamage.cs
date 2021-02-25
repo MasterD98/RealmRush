e9cf6cf2053b30f62f93007213df10488e8e315e
LAPTOP-EPH67RN9\ASUS <dasithrathnasinghe@gmail.com>
2021-02-25 19:25:54 +0530

    created EnemyDamage.cs to process Damage taken Enemy from Towers

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] int HitPoints=10;
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (HitPoints <= 1) { KillEnemy(); }
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void ProcessHit()
+    {
+        HitPoints--;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+    }
+}
