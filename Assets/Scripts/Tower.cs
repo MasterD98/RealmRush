a55490e724e2a791f4d94d4909138f78d000d64b
LAPTOP-EPH67RN9\ASUS <dasithrathnasinghe@gmail.com>
2021-02-25 19:47:02 +0530

    added maximum attack range to tower

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,27 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform TargetEnemy;
-
+    [SerializeField] float attackRange=30f;
     // Update is called once per frame
     void Update()
     {
-        objectToPan.LookAt(TargetEnemy);
+        if (TargetEnemy )
+        {
+            objectToPan.LookAt(TargetEnemy);
+            FireAtEnemy();
+            
+        }
+        else { Shoot(false); }
+    }
+
+    private void FireAtEnemy()
+    {
+        if (Vector3.Distance(gameObject.transform.position, TargetEnemy.position) <= attackRange) { Shoot(true);}
+    }
+
+    private void Shoot(bool isActive)
+    {
+        ParticleSystem.EmissionModule emission = GetComponentInChildren<ParticleSystem>().emission;
+        emission.enabled=isActive;
     }
 }
