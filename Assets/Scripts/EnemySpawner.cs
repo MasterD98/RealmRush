16a92c0a659e85fd9f4ce55d6def440b1bff90f8
LAPTOP-EPH67RN9\ASUS <dasithrathnasinghe@gmail.com>
2021-02-26 05:32:28 +0530

    added score and player health to game UI

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] float secondsBetweenSpawns=0.75f;
     [SerializeField] EnemyMovement enemyPrefab;
+    [SerializeField] Text score;
+    int enemies = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,7 @@ public class EnemySpawner : MonoBehaviour
         while (true) {
             EnemyMovement Enemy = Instantiate(enemyPrefab, new Vector3(-10, 15, 0), Quaternion.Euler(0,0,0));
             Enemy.transform.parent = FindObjectOfType<EnemySpawner>().transform;
+            score.text = (++enemies).ToString();
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
     }
---
+++ Assets/Scripts/PlayerBaseHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBaseHealth : MonoBehaviour
+{
+    [SerializeField] int baseHealth=100;
+    [SerializeField] int damagePerEnemy = 10;
+    [SerializeField] Text health; 
+    private void OnTriggerEnter(Collider other)
+    {
+        baseHealth -= damagePerEnemy;
+        health.text = baseHealth.ToString();
+    }
+
+    void Start()
+    {
+        health.text = baseHealth.ToString();    
+    }
+}
